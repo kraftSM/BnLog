88d113c3f5b895670b42740d4b5011c516c2bc0f
kraftSM <kraftSM@yandex.ru>
2023-11-03 17:56:35 +0300

    ++ IRepository...,  Repository. ++ IService...,  Service.

--- BLL/MappingProfile.cs
+++ BLL/MappingProfile.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using BnLog.DLL.Models.Entitys;
 using BnLog.DLL.Models.Security;
+using BnLog.DLL.Request;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,9 +14,9 @@ namespace BnLog.BLL
         public MappingProfile()
         {
             //CreateMap<RegisterViewModel, User>()
-            //CreateMap<RegisterRequest, User>()
-            //    .ForMember(x => x.Email, opt => opt.MapFrom(c => c.Email))
-            //    .ForMember(x => x.UserName, opt => opt.MapFrom(c => c.UserName));
+            CreateMap<RegisterRequest, User>()
+                .ForMember(x => x.Email, opt => opt.MapFrom(c => c.Email))
+                .ForMember(x => x.UserName, opt => opt.MapFrom(c => c.UserName));
 
             //CreateMap<CommentCreateRequest, Comment>();
             //CreateMap<CommentEditRequest, Comment>();
---
+++ BLL/Services/IService/IAccountService.cs
@@ -0,0 +1,20 @@
+﻿using BnLog.DLL.Models.Security;
+using BnLog.DLL.Models;
+using BnLog.DLL.Request;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
+
+namespace BnLog.BLL.Services.IService
+{
+    public interface IAccountService
+    {
+        Task<IdentityResult> Register(RegisterRequest model);
+        Task<SignInResult> Login(LoginRequest model);
+        Task<IdentityResult> EditAccount(UserEditRequest model);
+        //Task<UserEditRequest> EditAccount(Guid id);
+        Task RemoveAccount(Guid id);
+        Task<List<User>> GetAccounts();
+        Task LogoutAccount();
+    }
+}
